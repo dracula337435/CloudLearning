10cfd2c40d2d4397ec1de8880c517dc36e5cbdd5
dracula337435 <dracula337435@163.com>
2017-12-23 00:55:24 +0800

    gradle的spring-boot示例

---
+++ src/main/java/org/dracula/cloud/controller/TestController.java
@@ -0,0 +1,15 @@
+package org.dracula.cloud.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping(value="/test", method= RequestMethod.GET)
+    public String test(){
+        return "hello world";
+    }
+
+}
