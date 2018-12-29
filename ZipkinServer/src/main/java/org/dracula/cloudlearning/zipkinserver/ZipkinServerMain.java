4c7f67fad95fb8ac1f403c4c44bd21309282d2ce
dracula337435 <dracula337435@163.com>
2018-12-29 23:17:54 +0800

    另zipkin-server可用。仿照其可执行jar，写了自己可用的主类。配置文件换用yml。需要用zipkin的模块:2个client，zuul，采集概率改为0.5

--- ZipkinServer/src/main/java/org/dracula/cloudlearning/zipkinserver/ZipkinServerMain.java
+++ ZipkinServer/src/main/java/org/dracula/cloudlearning/zipkinserver/ZipkinServerMain.java
@@ -1,15 +1,23 @@
 package org.dracula.cloudlearning.zipkinserver;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import zipkin2.server.internal.EnableZipkinServer;
+import zipkin2.server.internal.RegisterZipkinHealthIndicators;
 
+/**
+ * spring-boot 2.1 很诡异地不能启动zipkin-server，但是发现下载的可执行jar中用的是spring-boot 2.1.1
+ * 于是仿照着其主类{@link zipkin.server.ZipkinServer}写一个主类
+ * @author dk
+ */
 @SpringBootApplication
 @EnableZipkinServer
 public class ZipkinServerMain {
 
     public static void main(String[] args){
-        SpringApplication.run(ZipkinServerMain.class, args);
+        new SpringApplicationBuilder(ZipkinServerMain.class)
+                .listeners(new RegisterZipkinHealthIndicators())
+                .properties("spring.config.name=zipkin-server").run(args);
     }
 
 }
